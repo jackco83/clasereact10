c23c7a700538bab8add695346e22f704f54131ae
Jack Cruzado Ocaris <jackco83@hotmail.com>
2022-03-22 10:17:43 -0500

    Add files via upload

---
+++ src/ItemListContainer.js
@@ -0,0 +1,40 @@
+import ItemList from "./ItemList"
+import { useEffect, useState } from "react"
+import { toast } from "react-toastify"
+import { useParams } from "react-router-dom"
+import productos from "./productos.json"
+
+
+const ItemListContainer = () => {
+
+    const [loading, setLoading] = useState(true)
+    const [productos, setProductos] = useState([])
+    const {idCategoria} = useParams()
+    
+    useEffect(() => {
+        
+        //fetch('/productosPublic.json') 
+        fetch('https://fakestoreapi.com/products')
+        .then((response)=>{
+            return response.json()
+        })
+        .then((resultado)=>{
+            setProductos(resultado)
+        })
+        .catch(()=>{
+            toast.error("Error al cargar los productos")
+        })
+        .finally(()=>{
+            setLoading(false)
+        })
+        
+    },[idCategoria])
+
+    if(loading){
+        return <h1>Cargando...</h1>
+    }else{
+        return <ItemList productos={productos}/>
+    }
+}
+
+export default ItemListContainer
